78d721cf90a619e92014d2290ce34488b3494163
Florian Marending <florian@marending.dev>
2025-06-25 22:33:09 +0200

    Change script

--- windows.sh
+++ windows.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-application=$(osascript -e 'tell application "System Events" to tell (first process whose frontmost is true) to return {name, name of window 1}')
+application=$(osascript -e 'tell application "System Events" to tell (first process whose frontmost is true) to return {name}')
+title=$(osascript -e 'tell application "System Events" to tell (first process whose frontmost is true) to return {name of window 1}')
 date=$(date +"%Y-%m-%dT%H:%M:%S")
 
-echo "$date,$application" >> /Users/florian/Projects/aware/windows.csv
+echo "$date,$application,$title" >> /Users/florian/Projects/aware/windows.csv
