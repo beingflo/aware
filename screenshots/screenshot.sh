b3a9ff974d5d96b449ba9e71ee07ef88b63676b1
Florian Marending <florian@marending.dev>
2025-06-29 18:51:23 +0200

    Finalize screenshot script

--- screenshots/screenshot.sh
+++ screenshots/screenshot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 date=$(date +"%Y-%m-%dT%H:%M:%S")
-filename="screenshot_$date.png"
+filename="screenshot_$date"
 
 screencapture "/Users/florian/Projects/aware/screenshots/data/screenshot.png"
-magick -quality 80% "/Users/florian/Projects/aware/screenshots/data/screenshot.png" "/Users/florian/Projects/aware/screenshots/data/$filename.jpg"
+/opt/homebrew/bin/magick -quality 80% "/Users/florian/Projects/aware/screenshots/data/screenshot.png" "/Users/florian/Projects/aware/screenshots/data/$filename.jpg"
 rm "/Users/florian/Projects/aware/screenshots/data/screenshot.png"
