bb57cb2ba42dbe2a76836f608506cbed4515b380
Florian Marending <florian@marending.dev>
2025-10-22 21:56:12 +0200

    Don't store screenshot if it's all black

--- screenshots/screenshot.sh
+++ screenshots/screenshot.sh
@@ -3,5 +3,10 @@ date=$(date +"%Y-%m-%dT%H:%M:%S")
 filename="screenshot_$date"
 
 screencapture "/Users/florian/Projects/aware/screenshots/data/screenshot.png"
-/opt/homebrew/bin/magick -quality 80% "/Users/florian/Projects/aware/screenshots/data/screenshot.png" "/Users/florian/Projects/aware/screenshots/data/$filename.jpg"
+
+mean=$(magick identify -format "%[fx:mean]" "/Users/florian/Projects/aware/screenshots/data/screenshot.png")
+if (( $(echo "$mean > 0.01" | bc -l) )); then
+  /opt/homebrew/bin/magick -quality 80% "/Users/florian/Projects/aware/screenshots/data/screenshot.png" "/Users/florian/Projects/aware/screenshots/data/$filename.jpg"
+fi
+
 rm "/Users/florian/Projects/aware/screenshots/data/screenshot.png"
