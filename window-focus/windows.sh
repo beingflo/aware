d2337824281ed4ac4672d2056f1300943d5c0750
Florian Marending <florian@marending.dev>
2025-10-22 21:42:28 +0200

    Don't record window if title is empty

--- window-focus/windows.sh
+++ window-focus/windows.sh
@@ -3,4 +3,6 @@ application=$(osascript -e 'tell application "System Events" to tell (first proc
 title=$(osascript -e 'tell application "System Events" to tell (first process whose frontmost is true) to return {name of window 1}')
 date=$(date +"%Y-%m-%dT%H:%M:%S")
 
-echo "$date,$application,$title" >> /Users/florian/Projects/aware/window-focus/windows.csv
+if [ -n "$title" ] || [ "$application" != "firefox" ]; then
+  echo "$date,$application,$title" >> /Users/florian/Projects/aware/window-focus/windows.csv
+fi
