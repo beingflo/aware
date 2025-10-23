8a26a852bd5d35185a2a687a38fc916679b2f71e
Florian Marending <florian@marending.dev>
2025-10-23 21:45:57 +0200

    Change threshold for black check

--- screenshots/screenshot.sh
+++ screenshots/screenshot.sh
@@ -5,7 +5,7 @@ filename="screenshot_$date"
 screencapture "/Users/florian/Projects/aware/screenshots/data/screenshot.png"
 
 mean=$(/opt/homebrew/bin/magick identify -format "%[fx:mean]" "/Users/florian/Projects/aware/screenshots/data/screenshot.png")
-if (( $(echo "$mean > 0.01" | bc -l) )); then
+if (( $(echo "$mean > 0.26" | bc -l) )); then
   /opt/homebrew/bin/magick -quality 80% "/Users/florian/Projects/aware/screenshots/data/screenshot.png" "/Users/florian/Projects/aware/screenshots/data/$filename.jpg"
 fi
 
